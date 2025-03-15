9193892f946ea4f6010663ea314a75b4b51b9f5e
Ahmed Osama <155636766+AhmdBlack0@users.noreply.github.com>
2025-03-15 15:23:26 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,14 +11,14 @@ app.get("/", (req, res) => {
   res.json("test24");
 });
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log("server work on port ", PORT);
-      console.log("content db");
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log("server work on port ", PORT);
+//       console.log("content db");
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
