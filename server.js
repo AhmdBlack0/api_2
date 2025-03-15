80167cc74f86efddb921ec561fe37bf2fa3d6ddc
Ahmed Osama <155636766+AhmdBlack0@users.noreply.github.com>
2025-03-15 15:11:52 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,15 +10,4 @@ app.get("/", (req, res) => {
   res.json("test2");
 });
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((error) => {
-    console.error("MongoDB connection error:", error);
-  });
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
