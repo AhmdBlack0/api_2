d035197ad4edc44f1b33d30e6b18b9bb8399304f
Ahmed Osama <155636766+AhmdBlack0@users.noreply.github.com>
2025-03-15 14:36:19 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Subscribe to Arpan Neupane's channel");
+  res.json("test");
 });
 
-app.listen(port, () => {
-  `Server started on port ${port}`;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
